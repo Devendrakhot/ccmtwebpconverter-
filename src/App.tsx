c9b2a10641738396168ceb72085bfe30b16b13aa
Devendrakhot <86885621+Devendrakhot@users.noreply.github.com>
2025-07-19 17:02:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,8 @@ function App() {
     const k = 1024;
     const sizes = ['Bytes', 'KB', 'MB', 'GB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(2) + ' ' + sizes[i];
+  return (bytes / Math.pow(k, i)).toFixed(2) + ' ' + sizes[i];
+
   };
 
   const handleDragOver = (e: React.DragEvent) => {
