8edd4f6c9189b4667517cfffdde8c3771b7e0388
Devendrakhot <86885621+Devendrakhot@users.noreply.github.com>
2025-07-22 11:15:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+     target: 'esnext', 
     rollupOptions: {
       external: ['@ffmpeg/ffmpeg']
     }
